d0fa588506437a172fcfa42d1d71373b5c8e0a21
Malgorzata Dudziec <dudziec.malgorzata@gmail.com>
2023-03-09 16:07:02 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { ref } from 'vue'
+import MyButton from '@/components/MyButton.vue'
+
+const loading = ref(false)
+
+const load = () => {
+  loading.value = true
+  setTimeout(() => (loading.value = false), 1000)
+}
+
+const appendText = ref('dodatkowa treść')
+</script>
+
+<template>
+  <my-button 
+      :loading="loading"
+      :disabled="loading"
+      color="primary"
+      size="small"
+      class="button"
+      @click="load"
+  >
+    <template #prepend>
+      <v-icon icon="mdi-checkbox-marked-circle" />
+    </template>
+
+    Przycisk
+
+    <template #append>
+      {{ appendText }}
+    </template>
+
+    <template #loader>
+      <v-icon light>mdi-cached</v-icon>
+    </template>
+  </my-button>
+</template>
---
+++ src/components/MyButton.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-btn v-bind="$attrs">
+    <template #prepend>
+      <slot name="prepend"></slot>
+    </template>
+
+    <slot></slot>
+
+    <template #append>
+      <slot name="append"></slot>
+    </template>
+  </v-btn>
+</template>
